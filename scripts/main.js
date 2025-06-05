1f549598601cd04cab2c88eeda3f95a8d3887a6a
thesyntaxdude <prince.akakpo@icloud.com>
2025-06-05 01:26:59 -0500

    Scripts: Added comments to explain value source

--- scripts/main.js
+++ scripts/main.js
@@ -16,13 +16,15 @@ function hexToRgb(hex) {
   return red, green, blue;
 }
 
-// console.log(hexToRgb(hex));
 
 function getLuminance(red, green, blue){
+    // this function converts the values of its arguments into sRGB.
+    // using values provided by the W3 WCAG 2.1 Contrast Guidelines. 
+
     red /= 255;
     green /= 255;
     blue /= 255;
-    
+
     red = (red <= 0.03928) ? red / 12.92 : (((red + 0.055) /1.055) ** 2.4);
     green = green <= 0.03928 ? green / 12.92 : ((green + 0.055) / 1.055) ** 2.4;
     blue = blue <= 0.03928 ? blue / 12.92 : ((blue + 0.055) / 1.055) ** 2.4;
@@ -30,5 +32,3 @@ function getLuminance(red, green, blue){
     const luminance = (0.2126 * red) + (0.7152 * green) + (0.0722 * blue);
     return luminance;
 }
-
-// console.log(getLuminance(220, 30, 40));
